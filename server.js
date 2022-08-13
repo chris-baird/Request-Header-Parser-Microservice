703301471a45bc3a92d7c0c47d6aa4d18c49ae4d
Chris Baird <cab3953@gmail.com>
2022-08-13 12:23:28 -0700

    Added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
-
+const cors = require('cors');
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use('/api/whoami', (req, res) => {
   let json = {
